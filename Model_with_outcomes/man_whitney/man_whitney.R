47a7d2b2ab8eb9166755d7bdcdee93662af8390a
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2018-06-22 11:52:39 +0530

    Added mann whitney for feature selection for with outcomes

---
+++ Model_with_outcomes/man_whitney/man_whitney.R
@@ -0,0 +1,40 @@
+data=read.csv("Documents/MS/COPD_Cluster_Analysis/Model_with_outcomes/man_whitney/dataset.csv")
+
+data=na.omit(data) #Add NA in the dataset for missing values(1 missing present)
+
+rownames(data)=data$id
+data=data[-1]
+
+#Splitting the data based clusters
+X=split(data,data$clusters)
+pop1=X$'1'
+pop2=X$'2'
+
+nam=colnames(data)
+
+
+#-----------Runnning the test for numerical------------
+for (i in nam[1:8])
+{p1=pop1[[i]]
+ p2=pop2[[i]]
+
+ t=wilcox.test(p1,p2,alternative = "two.sided",paired=FALSE)
+ #print(c(i,t$p.value))
+ if (t$p.value <= 0.05){print(i)}
+}
+#Plots
+#hist(p1,xlim=c(10,100),col='red')
+#hist(p2,xlim=c(10,100),add=T,col='blue')
+
+
+#rununing the test for categorical----------------
+for (i in nam[9:37])
+  {p1=pop1[[i]]
+   p2=pop2[[i]]
+  x=rbind(summary(p1),summary(p2))
+  #t=chisq.test(x,simulate.p.value = TRUE)
+  t=fisher.test(x,simulate.p.value = TRUE)
+  #print(c(i,t$p.value))
+  if (t$p.value <= 0.05){print(i)}
+  }
+
