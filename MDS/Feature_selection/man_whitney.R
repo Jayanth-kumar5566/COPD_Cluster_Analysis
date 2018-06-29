438f14852133157953581bcc9ac7732f7cdf2e8d
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2018-06-29 15:29:49 +0530

    Finalized the clusers, sent to pie yee for clinical evaluvation

---
+++ MDS/Feature_selection/man_whitney.R
@@ -0,0 +1,97 @@
+data=read.csv("Documents/MS/COPD_Cluster_Analysis/MDS/Feature_selection/dataset.csv")
+  
+data=na.omit(data) #Add NA in the dataset for missing values(1 missing present)
+rownames(data)=data$id
+data=data[-1]
+
+#Splitting the data based clusters
+X=split(data,data$x)
+
+pop1=X$'1'
+pop2=X$'2'
+pop3=X$`3`
+pop4=X$`4`
+pop5=X$'5'
+
+nam=colnames(data)
+
+
+#--------------For 3 Clusters Outcome Comparision-----------------------
+
+#Numerical
+for (i in nam[37])
+{p1=pop1[[i]]
+p2=pop2[[i]]
+p3=pop3[[i]]
+p4=pop4[[i]]
+p5=pop5[[i]]
+
+t=kruskal.test(list(p1,p2,p3,p4,p5))
+if (t$p.value <= 0.05){
+  print(i)
+  png(paste("./Documents/MS/COPD_Cluster_Analysis/MDS/Feature_selection/",i,".png",sep=''))
+  boxplot(p1,p2,p3,p4,p5,xlab=i)
+  dev.off()
+}
+}
+
+#Categorical
+for (i in nam[34:36])
+{p1=pop1[[i]]
+p2=pop2[[i]]
+p3=pop3[[i]]
+p4=pop4[[i]]
+p5=pop5[[i]]
+x=rbind(summary(p1),summary(p2),summary(p3),summary(p4),summary(p5))
+t=fisher.test(x,simulate.p.value = TRUE)
+if (t$p.value <= 0.05){print(i)
+  png(paste("./Documents/MS/COPD_Cluster_Analysis/MDS/Feature_selection/",i,".png",sep=''))
+  par(mfrow=c(1,5))
+  li=max(x)
+  plot(p1,xlab=i,ylim=c(1,li))
+  plot(p2,xlab=i,ylim=c(1,li))
+  plot(p3,xlab=i,ylim=c(1,li))
+  plot(p4,xlab=i,ylim=c(1,li))
+  plot(p5,xlab=i,ylim=c(1,li))
+  dev.off()}
+}
+
+
+#-----------------Cluster Characterization on 3-------------------
+
+#Numerical plots
+for (i in nam[1:7])
+{p1=pop1[[i]]
+p2=pop2[[i]]
+p3=pop3[[i]]
+p4=pop4[[i]]
+p5=pop5[[i]]
+t=kruskal.test(list(p1,p2,p3,p4,p5))
+if (t$p.value <= 0.05){
+  print(i)
+  png(paste("./Documents/MS/COPD_Cluster_Analysis/MDS/Feature_selection/",i,".png",sep=''))
+  boxplot(p1,p2,p3,p4,p5,xlab=i)
+  dev.off()
+}
+}
+
+#Categorical Plots
+for (i in nam[8:33])
+{p1=pop1[[i]]
+p2=pop2[[i]]
+p3=pop3[[i]]
+p4=pop4[[i]]
+p5=pop5[[i]]
+x=rbind(summary(p1),summary(p2),summary(p3),summary(p4),summary(p5))
+t=fisher.test(x,simulate.p.value = TRUE)
+if (t$p.value <= 0.05){print(i)
+  png(paste("./Documents/MS/COPD_Cluster_Analysis/MDS/Feature_selection/",i,".png",sep=''))
+  par(mfrow=c(1,5))
+  li=max(x)
+  plot(p1,xlab=i,ylim=c(1,li))
+  plot(p2,xlab=i,ylim=c(1,li))
+  plot(p3,xlab=i,ylim=c(1,li))
+  plot(p4,xlab=i,ylim=c(1,li))
+  plot(p5,xlab=i,ylim=c(1,li))
+  dev.off()}
+}
